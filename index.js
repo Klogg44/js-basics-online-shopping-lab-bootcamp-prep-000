7ba7aebcee928f55d0c573c57fd732b62706bd1b
Spencer Croy <spencerccroy@gmail.com>
2018-04-30 23:11:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ function total() {
 function removeFromCart(item) {
   for (let i=0; i<cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i,1)
-      return
+      return cart.splice(i,1)
     } else {
       return 'That item is not in your cart.'
     }
