b99f450c7a2b4a2d59bd1bbb2ac550c871a004e7
Spencer Croy <spencerccroy@gmail.com>
2018-04-30 23:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function placeOrder(cardNumber) {
     cart = []
     return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
   } else {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
 
