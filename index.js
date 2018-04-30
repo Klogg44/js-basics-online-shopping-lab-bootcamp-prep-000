ec07d8c7c4c9f48ed0eb2c17ee135a5670721705
Spencer Croy <spencerccroy@gmail.com>
2018-04-30 22:58:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,37 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var item = {
+    itemName: item,
+    itemPrice: Math.floor((Math.random() * 100) + 1)
+  };
+  cart.push(item);
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
+  if (cart.length > 0) {
+    let middle = []
+    for (let i=0; i<cart.length; i++) {
+      if (i === (cart.length - 1) && i !== 0) {
+        middle.push(` and ${cart[i].itemName} at $${cart[i].itemPrice}`)
+      } else {
+        middle.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
+      }
+    }
+    return `In your cart, you have${middle.join()}.`
+  } else {
+    return 'Your shopping cart is empty.'
+  }
   // write your code here
 }
 
 function total() {
-  // write your code here
+  let sum = 0
+  for (let i = 0; i < cart.length; i++){
+    sum += cart[i].itemPrice
+  }
+  return sum
 }
 
 function removeFromCart(item) {
